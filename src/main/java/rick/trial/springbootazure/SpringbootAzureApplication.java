b6405dbdf287977d503969b9d08b1b55cd7d4c54
Riccardo Bruno <mr.riccardo_bruno@libero.it>
2024-03-07 10:49:04 +0100

    first commit

---
+++ src/main/java/rick/trial/springbootazure/SpringbootAzureApplication.java
@@ -0,0 +1,27 @@
+package rick.trial.springbootazure;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@RestController
+@RequestMapping("/maintest")
+@SpringBootApplication
+public class SpringbootAzureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootAzureApplication.class, args);
+    }
+
+    @GetMapping("/message")
+    public Mono<String> message(){
+
+        return Mono.just("Hello Application deployed in  Azure !!!");
+    }
+
+}
